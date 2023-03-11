ef7fcd93ac3f8c8e97da77afce032b67837a5eef
Replit user <>
2023-03-11 15:43:39 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -81,6 +81,9 @@ typedef struct {
 } Vector;
 
 Vector * crearVector(int n) {
+  Vector *local = (Vector*) malloc(sizeof(Vector));
+  
+  
    return NULL;
 }
 
@@ -116,7 +119,8 @@ Ejercicio 8.
 Use las operaciones implementadas de vectores para 
 sumar (a1,a2)+(b1+b2). Almacene el resultado en el vector c.
 */
+
 void sumaV2(int a1, int a2, int b1, int b2, Vector *c){
-  c -> x = a1 + b1;
-  c -> y = a2 + b2;
+  c  = a1 + b1;
+  c  = a2 + b2;
 }
