8b3240d12979c41a42a40c984be0596fb4b43731
Replit user <>
2023-03-11 15:30:26 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -117,5 +117,7 @@ Use las operaciones implementadas de vectores para
 sumar (a1,a2)+(b1+b2). Almacene el resultado en el vector c.
 */
 void sumaV2(int a1, int a2, int b1, int b2, Vector *c){
-  fro(int i = 0)
+  for(int i = 0 ; i < 2 ; i++){
+    
+  }
 }
