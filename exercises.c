86d9906d189d975bbc6a2d286afb4a93214779c6
Replit user <>
2023-03-11 16:26:45 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -106,7 +106,7 @@ Programe la función int obtenerValor(Vector * v, int i),
 la cual retorna el valor en la posición i del vector v.
 */
 int obtenerValor(Vector * v, int i) {
-  
+
 }
 
 /*
@@ -115,8 +115,7 @@ Función que suma los vectores `a` y `b` y
 actualiza el vector `c` con el resultado de la suma.
 */
 void sumaV(Vector * a, Vector * b, Vector * c) {
-  int n = 3;
-  for(int i = 0 ; i < n ; i++){
+  for(int i = 0 ; i < a -> capacidad ; i++){
     c -> datos[i] = a -> datos[i] + b -> datos[i];
   }
 }
